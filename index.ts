9c137937ebf476a09e825bf5fd32c56a3b178c6e
KhanhNguyen <khanh.nguyenphanhuu@gmail.com>
2025-08-27 16:23:56 +0700

    create homepage (client)

---
+++ index.ts
@@ -0,0 +1,21 @@
+import express, {Request,Response} from 'express'
+import path from 'path'
+
+const app = express()
+const port = 3000
+
+// Thiết lập thư mục views và view engine pug
+app.set("views",path.join(__dirname,"views")); // Thư mục chứa file pug
+app.set("view engine","pug");                  // Thiết lập pug làm view engine
+
+// Thiết lập thư mục chứa file tĩnh
+app.use(express.static(path.join(__dirname,'public')));
+app.get("/",(req:Request,res:Response)=>{
+    res.render('client/pages/home',{
+        pageTitle: "Trang chủ"
+    })
+})
+
+app.listen(port,()=>{
+    console.log(`Website is running at port ${port}`)
+})
